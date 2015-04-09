4716b291540591f4cfa93ee49978f17c4c1316ea
Ijlal Khanzada <ijlalkhanzada@gmail.com>
2015-04-09 20:44:50 +0500

    Set Html of blog post project..

--- client/app/blog/blog.controller.js
+++ client/app/blog/blog.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('blogpostApp')
-  .controller('BlogCtrl', function ($scope) {
+  .controller('BlogCtrl', function ($scope,$http,$modal, $log) {
     $scope.message = 'Hello';
 
     /****************************************jQuery Function for toggle Nave Start**********************************/
@@ -10,8 +10,45 @@ angular.module('blogpostApp')
       $("#wrapper").toggleClass("toggled");
       console.log('Start Toggle Menu')
     });
+
       /****************************************jQuery Function for toggle Nave End**********************************/
 
+      $http.get('/api/blogs').success(function(awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+        console.log('Get Blog ):',awesomeThings)
+      });
+      $scope.addThing = function() {
+        if(!$scope.blogTextArea) {
+          console.log('adddthing');
+          return false;
+        }
+        $http.post('/api/blogs', { name: $scope.blogTextArea}).success( function(){
+          $http.get('/api/blogs').success(function(awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+            console.log('Get Blog ):',awesomeThings)
+          });
 
+        });
 
+        $scope.blogTextArea = '';
+        console.log('Save Blog ):',$scope.blogTextArea)
+      };
+      $scope.deleteThing = function() {
+      $http.delete('/api/blogs').success(function(){
+        console.log('ssssssssssss')
+      });
+    };
+
+    $scope.checkAll = function () {
+      console.log('selected');
+      if ($scope.selectedAll) {
+        $scope.selectedAll = true;
+
+      } else {
+        $scope.selectedAll = false;
+      }
+      angular.forEach($scope.awesomeThings,function(item){
+        item.Selected = $scope.selectedAll;
+      });
+    };
   });
--- server/api/blog/blog.model.js
+++ server/api/blog/blog.model.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 
 var BlogSchema = new Schema({
   name: String,
-  info: String,
+  blogTextArea: String,
   active: Boolean
 });
 
